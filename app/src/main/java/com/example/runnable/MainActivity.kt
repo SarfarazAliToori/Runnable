49d5994517e427228b47eccdca2331120a4ee780
SarfarazAliToori <alifarazzain@gmail.com>
2021-10-26 15:17:18 +0500

    Runnable done

--- app/src/main/java/com/example/runnable/MainActivity.kt
+++ app/src/main/java/com/example/runnable/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.runnable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var number : Int = 0
+    var handler : Handler = Handler()
+    var runnable : Runnable =  Runnable { }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnStartOnclick(view: View) {
+        number = 0
+        runnable = object : Runnable {
+            override fun run() {
+                tv_couter.text = "Timer : $number"
+                number++
+                tv_couter.text = "Timer : $number"
+                handler.postDelayed(this, 1000)
+            }
+
+        }
+        handler.post(runnable)
+
+        }
+
+
+    fun btnStopOnclick(view: View) {
+        handler.removeCallbacks(runnable)
+        number = 0
+        tv_couter.text = "Timer : $number"
+
+
     }
 }
